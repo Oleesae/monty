f89d761f1423c9319384e8177dc20fc86ac71cfc
victorpreston <prestonvictor25@gmail.com>
2023-12-03 12:47:38 -0800

    monty

--- main.c
+++ main.c
@@ -1,55 +1,45 @@
 #include "monty.h"
-
-stack_t *stack = NULL;
-char *n_value = NULL;
-unsigned int line_number = 1;
-char *command = NULL;
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - function
- * @argc: number of args
- * @argv: array of args
- *
- * Return: void
- */
-
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
+	char *content;
 	FILE *file;
-	char buffer[1024], *op;
-	stack_t *temp;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-	if (argc == 2)
-	{
-		file = fopen(argv[1], "r");
-		if (!file)
-		{
-			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-			exit(EXIT_FAILURE);
-		}
-
-		while (fgets(buffer, 1024, file) != NULL)
-		{
-			op = strtok(buffer, " \t\n");
-			command = op;
-			op = strtok(NULL, " \n\t");
-			n_value = op;
-			get_op();
-			line_number++;
-		}
-		fclose(file);
-	}
-	else
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	temp = stack;
-	while (stack)
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
 	{
-		temp = stack;
-		stack = temp->next;
-		free(temp);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-	return (EXIT_SUCCESS);
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+return (0);
 }
